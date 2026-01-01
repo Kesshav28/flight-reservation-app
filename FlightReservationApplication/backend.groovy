f8b4188c3452c4b524eabf962ddfc2ea64b303db
Kesshav28 <159225292+Kesshav28@users.noreply.github.com>
2026-01-01 16:45:30 +0530

    3 stages are added

--- FlightReservationApplication/backend.groovy
+++ FlightReservationApplication/backend.groovy
@@ -6,5 +6,23 @@ pipeline    {
                 git branch: 'main', url: 'https://github.com/Kesshav28/flight-reservation-app.git'
             }
         }
+        stage('Code-build') {
+            steps {
+                sh '''
+                cd FlightReservationApplication
+                mvn clean package
+                    '''
+            }
+        }
+        stage('QA-Test') {
+            steps {
+                withSonarQubeEnv(installationName: 'sonar', credentialsId: 'Sonar-token') {
+                    sh '''
+                    cd FlightReservationApplication
+                    mvn sonar:sonar -Dsonar.projectKey=flight-reservation
+                    '''
+                }
+            }
+        }
     }
 }
